1d4c3ed5ba2e61d0d309f58cc82f87dbd2e332fe
Dilshan Harendra Perera <52866622+DilshanHarendra@users.noreply.github.com>
2025-06-09 12:29:35 +0530

    bug fix

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -3,7 +3,7 @@ import Router from './router/Router.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { useEffect } from 'react';
 
-function App(props) {
+function App({props}) {
   useEffect(() => {
     console.log('data coming from parent to Dashboard', props.user);
   }, [props]);
